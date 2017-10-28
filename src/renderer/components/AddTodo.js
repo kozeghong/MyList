ec308a980886fff167cf5ae8e030f2bdf68302a3
Ken <10784731+kozeghong@users.noreply.github.com>
2017-10-28 21:31:17 +0800

    prop-types package

--- src/renderer/components/AddTodo.js
+++ src/renderer/components/AddTodo.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import styles from './addTodo.scss'
 
 export default class AddTodo extends Component {
--- src/renderer/components/TableList.js
+++ src/renderer/components/TableList.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import TableListItem from './TableListItem'
 import TableListAdd from './TableListAdd'
 import styles from './tableList.scss'
