21ded4492aca0986e9f2913669218ac6079d0bfe
Ken <10784731+kozeghong@users.noreply.github.com>
2017-11-17 01:50:25 +0800

    dev nightly

--- src/renderer/components/TodoEdit.js
+++ src/renderer/components/TodoEdit.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 
 export default class TodoEdit extends Component {
     render () {
+        const { text } = this.props
         return (
             <div>
+                <input
+                    type='text' ref='text'
+                    defaultValue={ text }
+                />
                 
             </div>
         )
