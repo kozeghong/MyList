dab0908c41a0e48d9cf6a4eb2ec663335cff3222
Ken <10784731+kozeghong@users.noreply.github.com>
2017-11-19 21:40:35 +0800

    dev nightly

--- src/renderer/components/TodoEdit.js
+++ src/renderer/components/TodoEdit.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styles from './todoEdit.scss'
+import SvgIcon from './SvgIcon'
 
 export default class TodoEdit extends Component {
     render () {
         const { text } = this.props
         return (
-            <div>
-                <input
-                    type='text' ref='text'
-                    defaultValue={ text }
-                />
-                
+            <div className={ styles.TodoEdit }>
+                <div>
+                    <SvgIcon icon='check-square' />
+                    <input
+                        type='text' ref='text'
+                        defaultValue={ text }
+                    />
+                </div>
+                <div>
+                    <input type="datetime-local" />
+                    <input type="datetime-local" />
+                </div>
+                <div>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <span>CREATED AT</span>
+                    <SvgIcon icon='trash-2' />
+                </div>
             </div>
         )
     }
